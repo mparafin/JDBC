e5c73940d8274019c8486fcb5d6f08dadf3066b1
Lab@ii.pw.edu.pl <mparafin@mion.elka.pw.edu.pl>
2018-04-20 15:37:40 +0200

    Initial no2

--- src/bd/OraConn.java
+++ src/bd/OraConn.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Test {
+public class OraConn {
+	
+	static Connection connection = null;
 
 	public static void main(String[] args) {
 		try {
@@ -13,17 +15,26 @@ public class Test {
 			System.out.println("Brak sterownika Oracle JDBC.");
 		}
 		System.out.println("Sterownik Oracle JDBC zosta³ zarejestrowany.");
-		Connection connection = null;
+		return;
+	}
+	
+	public static void open() {
 		try {
 			connection = DriverManager.getConnection("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf", "mparafin", "mparafin");
 		} catch (SQLException ex) {
-			System.out.println("B³¹d ³¹czenia z baz¹! " + ex.getMessage());
+			System.out.println("B³¹d ³¹czenia z baz¹! \nNumer b³êdu: " + ex.getErrorCode() + "\nOpis b³êdu: " + ex.getMessage());
 			return;
 		}
+		System.out.println("Po³¹czenie zosta³o otwarte.");
+		return;
+	}
+	
+	public static void close() {
 		try {
-			connection.close(); System.out.println("Po³¹czenie closed.");
+			connection.close();
+			System.out.println("Po³¹czenie zosta³o zamkniête.");
 		} catch (SQLException ex) {
-			ex.printStackTrace();
+			System.out.println("B³¹d zamykania po³¹czenia! \nNumer b³êdu: " + ex.getErrorCode() + "\nOpis b³êdu: " + ex.getMessage());
 		}
 	}
 
